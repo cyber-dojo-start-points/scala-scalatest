6d5770f148152a562a4a35abb1e63cd7e962cf46
JonJagger <jon@kosli.com>
2026-09-04 10:13:19 +0100

    Delete unneeded second test

--- start_point/HikerTest.scala
+++ start_point/HikerTest.scala
@@ -2,11 +2,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class HikerTest extends AnyFunSuite {
 
-  test("Example Test") {
+  test("life the universe and everything") {
     assert(Hiker.answer() == 42)
-  }
-
-  test("Other example test") {
-    assert("a" != "b")
   }
 }
